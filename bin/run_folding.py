b077f2fd64445e64f3514526b52d5f16ff247e44
Kate Cook <kate.cook@gmail.com>
2015-01-12 14:54:49 -0500

    add license

--- bin/run_folding.py
+++ bin/run_folding.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2014-2015 Kate Cook
+#
+# This file is part of rnascan.
+# 
+# rnascan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# rnascan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with rnascan.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 import os,sys,csv
 sys.path.append("..")
 import rnascan
