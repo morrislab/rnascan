ce75f3a3376db1b1cb39948862883ccc220869ac
Kevin Ha <kcha@users.noreply.github.com>
2016-09-08 15:27:44 -0400

    Fix indentation

--- RNACompete/secondarystructure.py
+++ RNACompete/secondarystructure.py
@@ -62,6 +62,6 @@ class RNAContextualSequenceSecondaryStructure(SecondaryStructure):
     def convert(cls, seqletter, structletter):
       """Return SeqStruct letter for given seqletter and structletter
       """
-        return cls.conversion[seqletter][structletter]
+      return cls.conversion[seqletter][structletter]
 
 
